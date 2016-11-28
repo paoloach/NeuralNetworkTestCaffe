ae23ed2e637ac9cfd1fb42a863e7da837b445812
paoloach <paolo.achdjian@skillbill.it>
2016-11-28 22:04:52 +0100

    work in progress

--- src/CheckAccurancy.cpp
+++ src/CheckAccurancy.cpp
@@ -15,13 +15,17 @@
 #include "caffe/util/benchmark.hpp"
 #include "caffe/util/io.hpp"
 #include "caffe/util/upgrade_proto.hpp"
+#include "caffe/proto/caffe.pb.h"
 #include "cuda.h"
 
 
-using std::vector
-using std::string
+using std::vector;
+using std::string;
 using namespace caffe;
 
+
+static std::string modelName ="./learn/net.prototxt";
+static int iterations=10000;
 
 
 // Parse GPU ids or use all available devices
@@ -39,33 +43,32 @@ static void get_gpus(vector<int>* gpus) {
 
 // Test: score a model.
 int test() {
-    vector<string> stages = get_stages_from_flags();
+    vector<string> stages ();
 
     // Set device id and mode
     vector<int> gpus;
     get_gpus(&gpus);
     if (gpus.size() != 0) {
-        LOG(INFO) << "Use GPU with device ID " << gpus[0];
 #ifndef CPU_ONLY
         cudaDeviceProp device_prop;
         cudaGetDeviceProperties(&device_prop, gpus[0]);
-        LOG(INFO) << "GPU device name: " << device_prop.name;
+        std::cout<< "GPU device name: " << device_prop.name;
 #endif
         Caffe::SetDevice(gpus[0]);
         Caffe::set_mode(Caffe::GPU);
     } else {
-        LOG(INFO) << "Use CPU.";
+        std::cout<< "Use CPU.";
         Caffe::set_mode(Caffe::CPU);
     }
     // Instantiate the caffe net.
-    Net<float> caffe_net(FLAGS_model, caffe::TEST, FLAGS_level, &stages);
-    caffe_net.CopyTrainedLayersFrom(FLAGS_weights);
-    LOG(INFO) << "Running for " << FLAGS_iterations << " iterations.";
+    Net<float> caffe_net(modelName, caffe::TEST);
+    //caffe_net.CopyTrainedLayersFrom(FLAGS_weights);
+    std::cout<< "Running for " << iterations << " iterations.";
 
     vector<int> test_score_output_id;
     vector<float> test_score;
     float loss = 0;
-    for (int i = 0; i < FLAGS_iterations; ++i) {
+    for (int i = 0; i < iterations; ++i) {
         float iter_loss;
         const vector<Blob<float>*>& result =
                                           caffe_net.Forward(&iter_loss);
@@ -83,28 +86,29 @@ int test() {
                 }
                 const std::string& output_name = caffe_net.blob_names()[
                         caffe_net.output_blob_indices()[j]];
-                LOG(INFO) << "Batch " << i << ", " << output_name << " = " << score;
+                std::cout<< "Batch " << i << ", " << output_name << " = " << score;
             }
         }
     }
-    loss /= FLAGS_iterations;
-    LOG(INFO) << "Loss: " << loss;
+    loss /= iterations;
+    std::cout<< "Loss: " << loss;
     for (int i = 0; i < test_score.size(); ++i) {
         const std::string& output_name = caffe_net.blob_names()[
                 caffe_net.output_blob_indices()[test_score_output_id[i]]];
         const float loss_weight = caffe_net.blob_loss_weights()[
                 caffe_net.output_blob_indices()[test_score_output_id[i]]];
         std::ostringstream loss_msg_stream;
-        const float mean_score = test_score[i] / FLAGS_iterations;
+        const float mean_score = test_score[i] / iterations;
         if (loss_weight) {
             loss_msg_stream << " (* " << loss_weight
                             << " = " << loss_weight * mean_score << " loss)";
         }
-        LOG(INFO) << output_name << " = " << mean_score << loss_msg_stream.str();
+        std::cout<< output_name << " = " << mean_score << loss_msg_stream.str();
     }
 
     return 0;
 }
 
 int main(int argc, char** argv) {
+    test();
 }
